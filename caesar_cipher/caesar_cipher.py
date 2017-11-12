64810954f2f8f4721623ef57c519b607e3e5fdef
Anthony Bendas <anthony.bendas@gmail.com>
2017-11-11 22:40:54 -0700

    first commit

---
+++ caesar_cipher/caesar_cipher.py
@@ -0,0 +1,19 @@
+import string
+
+print("Please enter a string to encrypt:")
+text = "Hello my name is Feythelus"
+print("Enter a shift factor:")
+shiftfactor = 5
+
+print(text)
+print(shiftfactor)
+
+def cipher(string, shift):
+  return True
+
+
+letters = list(string.ascii_lowercase[:26])
+capletters = list(string.ascii_uppercase[:26])
+
+print(letters)
+print(capletters)
