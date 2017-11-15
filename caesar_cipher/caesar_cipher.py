6eaeed35d97a01e723a7831020b95b67c3522024
Anthony Bendas <anthony.bendas@gmail.com>
2017-11-14 21:56:01 -0700

    completed script using repl and pasted in

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -1,19 +1,30 @@
+#import string for ascii
 import string
 
-print("Please enter a string to encrypt:")
-text = "Hello my name is Feythelus"
-print("Enter a shift factor:")
-shiftfactor = 5
+#store user string and shift factor
+plaintext = input("Please enter a sentence to encrypt: ")
+shift = int(input("Please enter a shift factor: "))
 
-print(text)
-print(shiftfactor)
+#cipher function
+def cipher(text, shiftfactor):
+  #init key lists
+  letters = list(string.ascii_lowercase[:26])
+  capletters = list(string.ascii_uppercase[:26])
+  ciphertext = ""
 
-def cipher(string, shift):
-  return True
+  #iterate through letter in string
+  for letter in text:
+    if letter in letters:
+      index = letters.index(letter)
+      index -= shiftfactor
+      ciphertext = ciphertext + (letters[index])
+    elif letter in capletters:
+      index = capletters.index(letter)
+      index -= shiftfactor
+      ciphertext = ciphertext + (capletters[index])
+    else:
+      ciphertext += letter
+  print (ciphertext)
 
-
-letters = list(string.ascii_lowercase[:26])
-capletters = list(string.ascii_uppercase[:26])
-
-print(letters)
-print(capletters)
+#call cipher
+cipher(plaintext, shift)
